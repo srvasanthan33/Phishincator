b683827cd2ce91cb80e443ea9decc027a4587b5f
Vasanthan <srvasanthan33@outlook.com>
2023-11-14 02:34:25 +0530

    a basic functionality is added for the working and can predict the url

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
+import pickle
 
 app = Flask(__name__)
+
+# Load the models
+with open('pipeline_with_LR_Tfid.pkl', 'rb') as model_file:
+    loaded_LR = pickle.load(model_file)
+with open('pipeline_with_MNB_Tfid.pkl', 'rb') as model_file:
+    loaded_MNB = pickle.load(model_file)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/predict',methods=["POST","GET"])
+def predict():
+    if request.method == 'GET':
+        return render_template('model_selection.html')
+    
+    # post method 
+    selected_model = request.form.get('model')
+    url = request.form.get('url')
+    if selected_model == 'lr':
+        prediction_result = loaded_LR.predict([url])[0]
+    elif selected_model == 'mnb':
+        prediction_result = loaded_MNB.predict([url])[0]
+    
+    sel_model = 'Logistic Regression' if selected_model == 'lr' else 'MultinomialNB'
+    return render_template('result.html',url=url,model=sel_model,result=prediction_result)
+    
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
