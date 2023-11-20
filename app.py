46fe04cac4473afbe356ff32e989f86e4e793833
Vasanthan <srvasanthan33@outlook.com>
2023-11-21 02:20:21 +0530

    DTC added as functionality in frontend as well as bacckend

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, url_for
+# Importing the pkl from models.py module
+from models import LR,MNB,DTC
 import pickle
 
 app = Flask(__name__)
@@ -8,6 +10,8 @@ with open('pipeline_with_LR_Tfid.pkl', 'rb') as model_file:
     loaded_LR = pickle.load(model_file)
 with open('pipeline_with_MNB_Tfid.pkl', 'rb') as model_file:
     loaded_MNB = pickle.load(model_file)
+with open('pipeline_with_DTC_Tfid.pkl', 'rb') as model_file:
+    loaded_DTC = pickle.load(model_file)
 
 @app.route('/')
 def home():
@@ -22,9 +26,11 @@ def predict():
     selected_model = request.form.get('model')
     url = request.form.get('url')
     if selected_model == 'lr':
-        prediction_result = loaded_LR.predict([url])[0]
+        prediction_result = LR().predict([url])[0]
     elif selected_model == 'mnb':
-        prediction_result = loaded_MNB.predict([url])[0]
+        prediction_result = MNB().predict([url])[0]
+    elif selected_model == 'dtc':
+        prediction_result = DTC().predict([url])[0]
     
     sel_model = 'Logistic Regression' if selected_model == 'lr' else 'MultinomialNB'
     return render_template('result.html',url=url,model=sel_model,result=prediction_result)
--- test_model.py
+++ test_model.py
@@ -1,1 +1,3 @@
-import pickle
+from models import LR,MNB,DTC
+
+print(DTC().predict(["http://localhost:8888/notebooks/phishing_url_prediction_model.ipynb"]))
